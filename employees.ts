9bf573d7f6df294b4bc8d17226b821d605a3575c
Abdul Khader Jeelani <abdulkj@thoughtworks.com>
2018-09-28 00:55:08 +0530

    [Abdul] Created sample for data transformation

---
+++ employees.ts
@@ -0,0 +1,20 @@
+export type EmployeeTransform = { role: string; quarter: number; country: string };
+
+export type Employee = {
+  grade: string;
+  country: string;
+  employment_type: string;
+  department: string;
+  offered_job_role: string;
+  start_date: Date;
+};
+
+export const employees = JSON.parse(`[
+    {
+        "grade": "Consultant",
+        "country": "China",
+        "employment_type": "Full-time",
+        "department": "Professional Services",
+        "offered_job_role": null,
+        "start_date": "2018-01-22"
+    }]`) as Employee[];
